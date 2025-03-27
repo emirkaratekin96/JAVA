c7ae291f6ff64d87bb6c5e63321da22bd57913c1
emir <karate791_aehit@students.vizja.pl>
2025-03-27 06:27:12 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Emir Karatekin!");
+        System.out.println("Emir Karatekin!? Yaren Sen");
     }
 }
